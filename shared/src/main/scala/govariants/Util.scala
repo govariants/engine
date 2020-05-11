8f297c5c534222840bc1bbfc5276c1971074a8a1
Gr0minet <bguevel@protonmail.com>
2020-05-11 23:00:00 +0200

    Add JSExport in Util.scala

--- shared/src/main/scala/govariants/Util.scala
+++ shared/src/main/scala/govariants/Util.scala
@@ -1,5 +1,9 @@
 package org.govariants.engine
 
+@JSExportTopLevel("Color")
 sealed trait Color
+
+@JSExportTopLevel("Black")
 case object Black extends Color
+@JSExportTopLevel("White")
 case object White extends Color
