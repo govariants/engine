486f6fd312d68be2096124444736c740c3cfdda3
Gr0minet <bguevel@protonmail.com>
2020-05-12 12:17:19 +0200

    Add an opposite attribut for Color trait
    Also fix a compilation error coming from the missing "import scalajs"

--- shared/src/main/scala/govariants/Util.scala
+++ shared/src/main/scala/govariants/Util.scala
@@ -1,9 +1,16 @@
 package org.govariants.engine
+import scalajs.js.annotation.{ JSExportTopLevel }
 
 @JSExportTopLevel("Color")
-sealed trait Color
+sealed trait Color {
+  val opposite: Color
+}
 
 @JSExportTopLevel("Black")
-case object Black extends Color
+case object Black extends Color {
+  val opposite: Color = White
+}
 @JSExportTopLevel("White")
-case object White extends Color
+case object White extends Color {
+  val opposite: Color = Black
+}
